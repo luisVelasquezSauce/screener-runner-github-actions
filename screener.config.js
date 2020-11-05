dc575eda3ad1aebd61551e0542b4a322ed60b451
Luis Hernandez <luis.velasquez@saucelabs.com>
2020-11-05 11:17:15 -0800

    Testing github actions

--- screener.config.js
+++ screener.config.js
@@ -1,12 +1,6 @@
 module.exports = {
-  // full repository name for your project:
   projectRepo: 'luis/github-actions-tests',
-
-  // this example assumes Environment Variables listed below exist on your system:
   apiKey: process.env.SCREENER_API_KEY,
-
-  // array of UI states to capture visual snapshots of.
-  // each state consists of a url and a name.
   states: [
     {
       url: 'https://screener.io',
